1aafa5e7e1d2f3300dec9fc27f7af555b96ef666
gabriel <metalmadnes98@gmail.com>
2022-04-22 23:42:51 -0500

    se agrego el requerimiento 2, se hizo uso de la funcion Date() para obtener la fecha

--- app/user.js
+++ app/user.js
@@ -1,11 +1,11 @@
 class user {
-    constructor(id, username, name, bio, dateCreated, lastUpdated) {
+    constructor(id, username, name, bio) {
         this.id = id
         this.username = username
         this.name = name
         this.bio = bio
-        this.dateCreated = dateCreated
-        this.lastUpdated = lastUpdated
+        this.dateCreated = new Date() // date nos da la hora del SO
+        this.lastUpdated = new Date()
     }
 }
 module.exports = user
--- test/user.test.js
+++ test/user.test.js
@@ -1,14 +1,25 @@
+// Importamos la clase que vamos a probar
 const User = require('./../app/user')
+    // creacion de los casos a probar
 describe("verificar creacion de ususarios", () => {
-    test('Case: creacion de usuario1', () => {
+    // Prueba de caso 1
+    test('Case1: creacion de usuario1', () => {
         // codigo que se desea probar
-        const user1 = new User(1, "ThatMan", "Gabriel", "Bio", "dateCreated", "lastUpdated")
-            // Resultado que se espera de las pruebas
+        const user1 = new User(1, "ThatMan", "Gabriel", "Bio")
+
+        // Resultado que se espera de las pruebas
         expect(user1.id).toBe(1);
         expect(user1.username).toBe("ThatMan");
         expect(user1.name).toBe("Gabriel");
         expect(user1.bio).toBe("Bio");
-        expect(user1.dateCreated).toBe("dateCreated");
-        expect(user1.lastUpdated).toBe("lastUpdated");
+    });
+    // prueba de caso 2
+    test('case2: dateCreated y lastUpdated guardan fecha al momento de instanciar', () => {
+        // codigo que se desea probar
+        const user2 = new User(1, "ThatMan", "Gabriel", "Bio")
+
+        // Resultado que se espera de las pruebas
+        expect(user2.dateCreated).not.toBeUndefined();
+        expect(user2.lastUpdated).not.toBeUndefined();
     });
 })
