1b13facb70f6e51772ebb629df8d4c69bcc7a9ab
gabriel <metalmadnes98@gmail.com>
2022-04-23 15:38:43 -0500

    se modifico el requisito 2 haciendo uso de la funcion object.values y asi poder acceder a ellos

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -10,6 +10,7 @@ class UserService {
         // El método Object.values() devuelve un array con los valores correspondientes a las propiedades enumerables
         // de un objeto. Las propiedades son devueltas en el mismo orden a como lo haría un bucle for...in 
 
+        // Se esta pasando por parametro el objeto user para despues con ayuda de Object.values obtener los valores
         return Object.values(user)
     }
 
