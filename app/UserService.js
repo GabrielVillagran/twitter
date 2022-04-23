d498052e4133407adce375feef8bd4c41c2454dd
gabriel <metalmadnes98@gmail.com>
2022-04-23 13:21:24 -0500

    se creo el metodo y la prueba para que se pudieran guardar la informacion de usuario en una lista, para esto se utilizo un metodo get

--- app/UserService.js
+++ app/UserService.js
@@ -5,9 +5,22 @@ class UserService {
         this.id = id
         this.username = username
         this.name = name
-            // this.bio = "user 26757213"
-        this.dateCreated = new Date() // date nos da la hora del SO
-        this.lastUpdated = new Date()
+        this.bio = "user 26757213"
+            // this.dateCreated = new Date() // date nos da la hora del SO
+            // this.lastUpdated = new Date()
+    }
+    get getInfo() {
+        // creacion de la lista
+        const listInfo = []
+
+        // se agregan los datos a la lista
+        listInfo.push(this.id)
+        listInfo.push(this.username)
+        listInfo.push(this.name)
+        listInfo.push(this.bio)
+
+        // Se retornan los valores que se agregaron a la lista
+        return listInfo
     }
 }
 module.exports = UserService
--- test/UserService.test.js
+++ test/UserService.test.js
@@ -16,4 +16,15 @@ describe("verificar creacion de ususarios", () => {
         console.log(user.dateCreated)
         console.log(user.lastUpdated)
     });
+    // Prueba de caso 2
+    test('Case2: metodo que recibe la clase user y me regresa una lista con todos los valores de los atributos', () => {
+        // codigo que se desea probar
+        const user = new userService(1, "ThatMan", "Gabriel")
+
+        // Resultado que se espera de las pruebas
+        expect(user.getInfo[0]).toBe(1);
+        expect(user.getInfo[1]).toBe("ThatMan");
+        expect(user.getInfo[2]).toBe("Gabriel");
+        expect(user.getInfo[3]).toBe("user 26757213");
+    });
 })
