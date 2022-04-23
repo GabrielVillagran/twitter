17b8f6080c20a41036433db7179dd8dcc584e4cf
gabriel <metalmadnes98@gmail.com>
2022-04-22 23:25:32 -0500

    Creacion de pruebas a utilizar

--- test/user.test.js
+++ test/user.test.js
@@ -1,14 +1,14 @@
-const user = require('./../app/user')
+const User = require('./../app/user')
 describe("verificar creacion de ususarios", () => {
     test('Case: creacion de usuario1', () => {
         // codigo que se desea probar
-        const user = new user(1, "ThatMan", "Gabriel", "Bio", "dateCreated", "lastUpdated")
+        const user1 = new User(1, "ThatMan", "Gabriel", "Bio", "dateCreated", "lastUpdated")
             // Resultado que se espera de las pruebas
-        expect(user.id).toBe(1);
-        expect(user.username).toBe("ThatMan");
-        expect(user.name).toBe("Gabriel");
-        expect(user.bio).toBe("Bio");
-        expect(user.dateCreated).toBe("dateCreated");
-        expect(user.dateUpdated).toBe("lastUpdated");
+        expect(user1.id).toBe(1);
+        expect(user1.username).toBe("ThatMan");
+        expect(user1.name).toBe("Gabriel");
+        expect(user1.bio).toBe("Bio");
+        expect(user1.dateCreated).toBe("dateCreated");
+        expect(user1.lastUpdated).toBe("lastUpdated");
     });
 })
